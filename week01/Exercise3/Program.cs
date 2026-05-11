813cbca8c1900004ed4ed760b0c3a36eeb5b596e
Naomi <naomisb2020@gmail.com>
2026-05-11 18:48:50 -0300

    Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 101);
+
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string result = Console.ReadLine();
+            guess = int.Parse(result);
+            if(guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+                
+            }
+            else if(guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+                
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                
+            } 
+        }while (guess != magic_number);
     }
 }
