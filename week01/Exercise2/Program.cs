95a705fdba88e268113240fc78511d7ce5cc3294
Naomi <naomisb2020@gmail.com>
2026-05-10 23:54:33 -0300

    Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("\nWhat is your grade porcentage? ");
+        string calification = Console.ReadLine();
+        int percent = int.Parse(calification);
+
+        if (percent >= 90)
+        {
+           Console.WriteLine("Your grade is A");
+        }
+
+        else if (percent >= 80 && percent < 90)
+        {
+            Console.WriteLine("Your grade is B"); 
+        }
+
+        else if (percent >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+    
+
+        else if (percent >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulation you past the cours!");
+        }
+        else
+        {
+           Console.WriteLine("Better luck next time!"); 
+        }
     }
 }
