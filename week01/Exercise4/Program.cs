074bc64c27947ba40b0983394a4d0d19f423afd5
Naomi <naomisb2020@gmail.com>
2026-05-11 20:15:59 -0300

    Exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int user_number = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (user_number != 0)
+        {
+            Console.Write("Enter number: ");
+            string number = Console.ReadLine();
+            user_number = int.Parse(number);
+
+            if (user_number != 0)
+            {
+                numbers.Add(user_number);
+            }
+
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        
+        int largest = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {largest}");
     }
 }
