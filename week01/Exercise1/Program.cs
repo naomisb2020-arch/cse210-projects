c70104ebfe515dc658ee46b60b4459208413bf99
Naomi <naomisb2020@gmail.com>
2026-05-10 23:51:49 -0300

    Exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        
+        Console.Write("\nWhat is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        
+        Console.WriteLine($"\nYour name is {last_name}, {first_name} {last_name}.");
     }
 }
