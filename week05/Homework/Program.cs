fd38944c330b48922f56cad84be66320518eb59b
Naomi <naomisb2020@gmail.com>
2026-06-06 15:04:49 -0300

    Week 05 Homework Program

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathTask = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "3-10, 20-21");
+
+        Console.WriteLine(mathTask.GetSummary());
+
+        Console.WriteLine(mathTask.GetHomeworkList());
+
+
+
+        WritingAssignment writingTask = new WritingAssignment("Mary Waters", "European History", "The Causes of WWII");
+
+        Console.WriteLine(writingTask.GetSummary());
+
+        Console.WriteLine(writingTask.GetWritingInformation());
+
     }
 }
