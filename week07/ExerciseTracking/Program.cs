e951f929162c8de4aa7d0c8482b5c29f947d7b51
Naomi <naomisb2020@gmail.com>
2026-06-17 21:30:25 -0300

    W07 Assignment: Exercise Tracking Program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running runningActivity = new Running("17 Jun 2026", 30, 4.8);
+        Cycling cyclingActivity = new Cycling("18 Jun 2026", 45, 20.0);
+        Swimming swimmingActivity = new Swimming("19 Jun 2026", 25, 30);
+
+        activities.Add(runningActivity);
+        activities.Add(cyclingActivity);
+        activities.Add(swimmingActivity);
+
+        Console.WriteLine("Exercise Tracking Summary:");
+        Console.WriteLine("--------------------------");
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
